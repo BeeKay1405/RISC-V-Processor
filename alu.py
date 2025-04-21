6e55ee94e9bb0fe803d899df9e8424fe427d1a92
BeeKay1405 <akshat01400@gmail.com>
2025-04-21 21:08:55 +0530

    - Added more functions in alu

--- alu.py
+++ alu.py
@@ -1,13 +1,49 @@
+# alu.py
+# RV64I + M-extension ALU implementation
+
+MASK64 = (1 << 64) - 1
+
+
 def alu(op1, op2, alu_op):
-    if alu_op == 'add': return op1 + op2
-    if alu_op == 'sub': return op1 - op2
-    if alu_op == 'mul': return op1 * op2
-    if alu_op == 'and': return op1 & op2
-    if alu_op == 'or':  return op1 | op2
-    if alu_op == 'xor': return op1 ^ op2
-    if alu_op == 'sll': return op1 << (op2 & 0x3F)
-    if alu_op == 'srl': return (op1 % (1 << 64)) >> (op2 & 0x3F)
-    if alu_op == 'sra': return op1 >> (op2 & 0x3F)
-    if alu_op == 'slt': return int(op1 < op2)
-    if alu_op == 'sltu': return int((op1 % (1 << 64)) < (op2 % (1 << 64)))
+    op1_u = op1 & MASK64
+    op2_u = op2 & MASK64
+
+    if alu_op == 'add':   return (op1 + op2) & MASK64
+    if alu_op == 'sub':   return (op1 - op2) & MASK64
+    if alu_op == 'mul':   return (op1 * op2) & MASK64
+    if alu_op == 'mulh':  return ((op1 * op2) >> 64) & MASK64
+    if alu_op == 'mulhu': return ((op1_u * op2_u) >> 64) & MASK64
+    if alu_op == 'mulhsu': return ((op1 * op2_u) >> 64) & MASK64
+
+    if alu_op == 'div':
+        if op2 == 0:
+            return -1 & MASK64
+        if op1 == -2 ** 63 and op2 == -1:
+            return op1  # overflow case
+        return int(op1 // op2) & MASK64
+
+    if alu_op == 'divu':
+        return (op1_u // op2_u) & MASK64 if op2 != 0 else MASK64
+
+    if alu_op == 'rem':
+        if op2 == 0:
+            return op1 & MASK64
+        if op1 == -2 ** 63 and op2 == -1:
+            return 0
+        return (op1 % op2) & MASK64
+
+    if alu_op == 'remu':
+        return (op1_u % op2_u) & MASK64 if op2 != 0 else op1_u
+
+    if alu_op == 'and':   return (op1 & op2) & MASK64
+    if alu_op == 'or':    return (op1 | op2) & MASK64
+    if alu_op == 'xor':   return (op1 ^ op2) & MASK64
+
+    if alu_op == 'sll':   return (op1 << (op2 & 0x3F)) & MASK64
+    if alu_op == 'srl':   return (op1_u >> (op2 & 0x3F)) & MASK64
+    if alu_op == 'sra':   return (op1 >> (op2 & 0x3F)) & MASK64
+
+    if alu_op == 'slt':   return int(op1 < op2)
+    if alu_op == 'sltu':  return int(op1_u < op2_u)
+
     return 0
--- execute_stage.py
+++ execute_stage.py
@@ -1,9 +1,10 @@
 from control_unit import get_control_signals
 from alu import alu
-from utils import get_immediate  
+from utils import imm  # from your Phase 2
 from register_file import RegisterFile
 
 def execute(decoded_inst, reg_file):
+    # Decoded instruction has: mnemonic, rs1, rs2, rd, opcode, funct3, funct7
     signals = get_control_signals(decoded_inst.mnemonic)
     rs1_val, rs2_val = reg_file.read(decoded_inst.rs1, decoded_inst.rs2)
     imm = get_immediate(decoded_inst)
